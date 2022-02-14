360ece32b1c60bd44012752c828c52c7fedea2cb
Dane Walker <dane@danecwalker.com>
2022-02-14 11:51:13 +1000

    Initial Commit

---
+++ AssignmentTimeCalculator/AssignmentTimeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentTimeCalculatorApp.swift
+//  AssignmentTimeCalculator
+//
+//  Created by Dane Walker on 14/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentTimeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentTimeCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AssignmentTimeCalculator
+//
+//  Created by Dane Walker on 14/2/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
